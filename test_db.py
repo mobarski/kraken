ac5f9cdbc8e50c5163ab2679111ef155ec7ed504
Maciej Obarski <mobarski@gmail.com>
2023-07-13 23:34:26 +0200

    segments

--- test_db.py
+++ test_db.py
@@ -5,17 +5,18 @@ from random import choice,randint
 import sim
 
 POOL = [1,2,3]
+SEG = ['gender']
 CTX = sim.random_ctx(sim.ctx_config)
 CTX_PER_ID = [{'pos':1},{'pos':2},{'pos':3}]
 
-db.register_views(POOL, ctx=CTX, ctx_per_id=CTX_PER_ID)
+db.register_views(POOL, ctx=CTX, ctx_per_id=CTX_PER_ID, seg=SEG)
 i = randint(0,len(POOL)-1)
 a = POOL[i]
-db.register_click(a, ctx=CTX, ctx2=CTX_PER_ID[i])
-db.calculate_ctr(POOL, ctx=CTX)
-db.calculate_ucb(POOL, ctx=CTX)
+db.register_click(a, ctx=CTX, ctx2=CTX_PER_ID[i], seg=SEG)
+db.calculate_ctr(POOL, ctx=CTX, seg=SEG)
+db.calculate_ucb(POOL, ctx=CTX, seg=SEG)
 pprint(dict(db.db.items()))
 
-pprint(db.sorted_by_stat('ctr',POOL,ctx=CTX))
-pprint(db.sorted_by_stat('ucb',POOL,ctx=CTX))
+pprint(db.sorted_by_stat('ctr', POOL, ctx=CTX, seg=SEG))
+pprint(db.sorted_by_stat('ucb', POOL, ctx=CTX, seg=SEG))
 print(CTX)
